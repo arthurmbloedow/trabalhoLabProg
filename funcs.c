83c38fba05f12146607c93a609293c3ff587b570
arthurmbloedow <44561056+arthurmbloedow@users.noreply.github.com>
2018-10-29 20:16:52 -0300

    via upload
    o programa agora escreve e lê no "filmes_data.txt"

--- funcs.c
+++ funcs.c
@@ -9,39 +9,62 @@ void cadastrarFilme()
 {
 	clear();
 	
-	char codigo[60];
-    	printf("Digite o codigo do filme: ");
+	char codigo[10];
+    printf("Digite o codigo do filme: ");
 	fgetc(stdin);
 	fgets(codigo, sizeof(codigo), stdin);
+	strtok(codigo, "\n");
+
+	while (checkCodigoDuplicado(codigo) == 1)
+	{
+		printf("Codigo ja existe! Digite um novo codigo: ");
+		fgets(codigo, sizeof(codigo), stdin);
+		strtok(codigo, "\n");
+	}
 
 	char nome[60];
 	printf("Digite o nome do filme: ");
+	//fgetc(stdin);
 	fgets(nome, sizeof(nome), stdin);
+	strtok(nome, "\n");
 
 	char locado;
 	while (locado != 's' && locado != 'n')
 	{
 		printf("O filme esta locado? [s/n]: ");
-		scanf("%c", &locado);
+		scanf(" %c", &locado);
 	}
- 
+
+	strcat(codigo, ";");
+	strcat(codigo, nome);  //n pergunte o pq
+	strcpy(nome, codigo);  
 
 	if (locado == 's')
 	{
 		int dias = 0;
-		char sdias[66];
+		char sdias[3];
 		while (dias <= 0)
 		{
 			printf("O filme esta locado ha quantos dias? ");
 			scanf("%d", &dias);
 		}
 		sprintf(sdias, "%d;", dias);
+		strcat(nome, ";true;");
+		strcat(nome, sdias);
 
-		char nomeCliente[100];
+		char nomeCliente[30];
 		printf("Qual cliente locou o filme? ");
 		fgetc(stdin);
 		fgets(nomeCliente, sizeof(nomeCliente), stdin);
+		strcat(nome, nomeCliente);
 	}
+	else
+		strcat(nome, ";false;0;no_client\n");
+
+	FILE *fptr;
+	fptr = fopen("filmes_data.txt","a+");
+	fprintf(fptr,"%s", nome);
+	fclose(fptr);
 
 	printf("\nFilme cadastrado com sucesso! \n");
 	sleep(1);
@@ -100,6 +123,7 @@ void editarFilme()
 	}
 }
 
+
 void procurarFilme()
 {
 	clear();
@@ -115,11 +139,61 @@ void procurarFilme()
 
 void listarFilmes()
 {
+	fgetc(stdin);
 	clear();
+	FILE* file = fopen("filmes_data.txt", "r");
 
-    	printf("Codigo; Nome do filme; Status \n");
-    	printf("--------------------------------------------------------------------------------\n\n");
+    char line[120];
+    char print[120] = "";
 
+    printf("Codigo; Nome do filme; Status \n");
+    printf("--------------------------------------------------------------------------------\n\n");
+
+	while (fgets(line, sizeof(line), file)) 
+	{
+		//printf("%s", line);
+		int i = 0;
+		char * str;
+
+		str = strtok(line, ";"); //codigo
+		strcat(print, "[");
+		strcat(print, line);
+		strcat(print, "] ");
+
+		str = strtok(NULL, ";"); //nome
+		strcat(print, str);
+		strcat(print, " esta ");
+
+		str = strtok(NULL, ";"); //locado (true false)
+		if (strcmp(str, "true") == 0)
+		{
+			str = strtok(NULL, ";");
+			strcat(print, "locado ha ");
+			strcat(print, str);
+			int multa = atoi(str);
+			strcat(print, " dia(s) por ");
+			str = strtok(NULL, ";");
+			strtok(str, "\n");
+			strcat(print, str);
+			if (multa > MULTA_DIAS)
+			{
+				multa = (multa - MULTA_DIAS) * PRECO_PADRAO;
+				strcat(print, ", com multa de R$ ");
+				char smulta[5];
+				sprintf(smulta, "%d", multa);
+				strcat(print, smulta);
+			}
+			strcat(print, ".");
+		}
+		else
+			strcat(print, "disponivel.");
+
+		printf("%s \n", print);
+		i = 0;
+		strcpy(print, "");
+    }
+
+    fclose(file);
 	printf("\n\nPressione Enter para continuar...");
 	fgetc(stdin);
 }
@@ -128,5 +202,4 @@ void gerarRelatorio() //pdf
 {
 	
 }
-
 
