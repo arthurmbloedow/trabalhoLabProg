051c170e2b7f0582fba25f4165a1c361d04aa1e9
arthurmbloedow <44561056+arthurmbloedow@users.noreply.github.com>
2018-10-29 15:03:16 -0300

    via upload
    opções de editarFilme();

--- funcs.c
+++ funcs.c
@@ -10,7 +10,7 @@ void cadastrarFilme()
 	clear();
 	
 	char codigo[60];
-    printf("Digite o codigo do filme: ");
+    	printf("Digite o codigo do filme: ");
 	fgetc(stdin);
 	fgets(codigo, sizeof(codigo), stdin);
 
@@ -49,7 +49,55 @@ void cadastrarFilme()
 
 void editarFilme()
 {
-	
+	clear();
+
+	char pesquisa[256];
+	printf("Digite o nome/codigo do filme que quer editar: ");
+	fgetc(stdin);
+	fgets(pesquisa, sizeof(pesquisa), stdin);
+	strtok(pesquisa, "\n");	
+
+	int found = 0;
+
+	if (found == 0)
+	{
+		printf("A pesquisa \"%s\" nao encontrou resultados", pesquisa);
+	}
+	else
+	{
+		printf("\nO que quer editar? ");
+		printf("\n1 : Codigo");
+		printf("\n2 : Nome");
+		printf("\n3 : Status");
+		printf("\n0 : Voltar");
+		printf("\nInput: ");
+
+		int y = 0;
+		scanf("%d", &y);
+
+		if (y==1)
+		{
+			char new_code[60];
+			printf("\nDigite o novo codigo: ");
+			fgetc(stdin);
+			fgets(new_code, sizeof(new_code), stdin);
+		}		
+		else if (y==2)
+		{
+			char new_name[256];
+			printf("\nDigite o novo nome: ");
+			fgetc(stdin);
+			fgets(new_name, sizeof(new_name), stdin);
+		}	
+		else if (y==3)
+		{
+			printf("\nO que quer editar? ");
+			printf("\n1 : Alterar o status de locacao");
+			printf("\n0 : Voltar");
+			printf("\nInput: ");
+			scanf("%d", &y);
+		}
+	}
 }
 
 void procurarFilme()
@@ -69,8 +117,8 @@ void listarFilmes()
 {
 	clear();
 
-    printf("Codigo; Nome do filme; Status \n");
-    printf("--------------------------------------------------------------------------------\n\n");
+    	printf("Codigo; Nome do filme; Status \n");
+    	printf("--------------------------------------------------------------------------------\n\n");
 
 	printf("\n\nPressione Enter para continuar...");
 	fgetc(stdin);
@@ -80,4 +128,5 @@ void gerarRelatorio() //pdf
 {
 	
 }
+
 
